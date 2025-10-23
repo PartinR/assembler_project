cce3c201c15f98f4ff5a78234fa5b9524406d533
Race Partin <racepartin@gmail.com>
2025-10-23 11:57:30 -0400

    Update 'Assem.cpp' with the correct file path for 'stdafx.h'

--- src/Assem.cpp
+++ src/Assem.cpp
@@ -1,7 +1,7 @@
 /*
  * Assembler main program.
  */
-#include "../stdafx.h"     // This must be present if you use precompiled headers which you will use. 
+#include "../include/stdafx.h"     // This must be present if you use precompiled headers which you will use. 
 #include <stdio.h>
 
 #include "Assembler.h"
