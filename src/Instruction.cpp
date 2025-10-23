716b77c79961ecae0b76ebd56bab34ade29c75b8
Race Partin <racepartin@gmail.com>
2025-10-23 12:46:21 -0400

    Create 'Instruction.cpp' and update with template

---
+++ src/Instruction.cpp
@@ -0,0 +1,29 @@
+#include "../include/stdafx.h"
+#include "../include/Instruction.h"
+#include "../include/Errors.h"
+
+string RemoveComment(string line) {
+    size_t pos = line.find(';');
+    if (pos == string::npos)
+    {
+        return line;
+    }
+    return line.erase(pos);
+}
+// Will parse a line into label, op code, and operand.
+bool ParseLine(const string& line, string& label, string& opcode, string& operand1, string &operand2)
+{
+    istringstream ins(line);
+    label = opcode = operand1 = operand2 = "";
+    if (line.empty()) return true;
+
+    string extra;
+
+    if (line[0] != ' ' && line[0] != '\t')
+    {
+        ins >> label;
+    }
+    ins >> opcode >> operand1 >> operand2 >> extra;
+
+    return extra == "";
+}
